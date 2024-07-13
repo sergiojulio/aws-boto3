1a5b0a1d009a45a17f64b7ebcc7ab8bffef63aee
sergiojulio <sergiojulio@gmail.com>
2024-07-13 19:56:26 -0400

    changes

--- extract_mysql_incremental.py
+++ extract_mysql_incremental.py
@@ -2,7 +2,41 @@ import pymysql
 import csv
 import boto3
 import configparser
+import psycopg2
 
+
+
+# get db Redshift connection info
+parser = configparser.ConfigParser()
+parser.read("pipeline.conf")
+dbname = parser.get("postgres_config", "database")
+user = parser.get("postgres_config", "username")
+password = parser.get("postgres_config", "password")
+host = parser.get("postgres_config", "host")
+port = parser.get("postgres_config", "port")
+
+# connect to the postgres_config cluster
+rs_conn = psycopg2.connect(
+                            "dbname=" + dbname
+                            + " user=" + user
+                            + " password=" + password
+                            + " host=" + host
+                            + " port=" + port)
+
+rs_sql = """SELECT COALESCE(MAX(lastUpdated),'1900-01-01') FROM orders;"""
+
+rs_cursor = rs_conn.cursor()
+rs_cursor.execute(rs_sql)
+result = rs_cursor.fetchone()
+
+# there's only one row and column returned
+last_updated_warehouse = result[0] # <-
+
+rs_cursor.close()
+rs_conn.commit()
+
+
+# get the MySQL connection info and connect
 parser = configparser.ConfigParser()
 
 parser.read("pipeline.conf")
@@ -24,11 +58,15 @@ if conn is None:
 else:
     print("MySQL connection established!")
 
-m_query = "SELECT * FROM Orders;"
+m_query = """SELECT *
+             FROM Orders
+             WHERE LastUpdated > %s;"""
+
 local_filename = "order_extract.csv"
 
 m_cursor = conn.cursor()
-m_cursor.execute(m_query)
+#m_cursor.execute(m_query)
+m_cursor.execute(m_query, (last_updated_warehouse,))
 
 results = m_cursor.fetchall()
 
@@ -52,4 +90,7 @@ s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secr
 s3_file = local_filename
 
 s3.upload_file(local_filename, bucket_name, s3_file)
+
+
+
 
